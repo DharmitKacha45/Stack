94b0e5e562a51cdd0327ef3055bc7a2408d5a06b
DharmitKacha45 <dharmitkacha8@gmail.com>
2026-09-15 14:31:15 +0530

    Update Stack_operation.c

--- Stack_operation.c
+++ Stack_operation.c
@@ -34,36 +34,6 @@ void push( struct stack* ptr,int val){
     }
 }
 
-int pop(struct stack*ptr){
-    if(isempty(ptr)){
-        printf("stack underflow! cannot pop from the stack \n");
-        return -1;
-    }
-    else{
-        int val=ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
-    }
-}
-
-int peek(struct stack*sp,int i){
-    int arrayind=sp->top-i+1;
-    if(arrayind <0){
-        printf("not a valid position for the stack\n");
-        return -1;
-    }
-    else{
-        return sp->arr[arrayind];
-    }
-}
-
-int stacktop(struct stack*sp){
-    return sp->arr[sp->top];
-}
-
-int stackbottom(struct stack*sp){
-    return sp->arr[0];
-}
 int main(){
 struct stack *sp=(struct stack*)malloc(sizeof(struct stack));
 sp->size=10;
@@ -86,25 +56,5 @@ push(sp,83546);
 
 printf(" after pushing, full:%d \n",isfull(sp));
 printf(" after pushing, empty:%d \n",isempty(sp));
-
-/*printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));
-printf("popped %d from the stack\n ",pop(sp));*/
-
-//printing values from the stack
-
-/*for(int j=1; j <= sp->top + 1 ;j++){
-    printf("the value at position  %d is %d \n",j,peek(sp,j));
-}*/
-
-printf("the value of top of stack is %d \n",stacktop(sp));
-printf("the value of bottom of stack is %d \n",stackbottom(sp));
     return 0;
 }
